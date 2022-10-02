fb24a743efe46d5cdfcea226cfbc14a695d233b7
irvanridho <irvanridho@live.com>
2022-10-02 17:58:06 +0700

    added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shippiiplugcustomer
+
+Shippii Plug for Customer
---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(($) => {
+  $(".quantity").on("click", ".plus", function (e) {
+    let $input = $(this).prev("input.qty");
+    let val = parseInt($input.val());
+    $input.val(val + 1).change();
+  });
+
+  $(".quantity").on("click", ".minus", function (e) {
+    let $input = $(this).next("input.qty");
+    var val = parseInt($input.val());
+    if (val > 0) {
+      $input.val(val - 1).change();
+    }
+  });
+});
